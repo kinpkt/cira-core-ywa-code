0e00d5555f27fbd8dbb6892bf4967e683ecd4c44
KinCubing <phakhinthon@gmail.com>
2023-01-07 22:14:03 +0700

    Line Drawing Code is done, waiting for finalized calculation alg.

--- line_drawing.py
+++ line_drawing.py
@@ -17,6 +17,16 @@ def halfpoint(coor1, coor2):
     half_coor = (int((coor1[0]+coor2[0])/2), int((coor1[1]+coor2[1])/2))
     return half_coor
 
+def drawline(key1, key2):
+    if key1 in obj_names:
+        parts_in[key1] = True
+    if key2 in obj_names:
+        parts_in[key2] = True
+    try:
+        cv2.line(img, coor_dict[key1], coor_dict[key2], LINE_COLOR, 10)
+    except:
+        pass
+
 obj_count = payload['DeepDetect']['count']
 obj_list = [d for d in payload['DeepDetect']['objects']]
 obj_names = payload['DeepDetect']['obj_count_str'].split(' : 1,')
@@ -30,7 +40,14 @@ parts_in = {'Hand': False, 'Foot': False, 'Butt': False, 'Head': False, 'Knee': 
 for i in obj_list:
     coor_dict[i['name']] = (i['x'], i['y'])
    
-print(coor_dict)
-   
+drawline('Head', 'Shoulder')
+drawline('Shoulder', 'Hand')
+drawline('Hand', 'Elbow')
+drawline('Shoulder', 'Butt')
+drawline('Butt', 'Knee')
+drawline('Knee', 'Foot')
+
 for i in coor_dict:
     cv2.circle(img, coor_dict[i], 15, POINT_COLOR, -1)
+
+setGlobal('parts_in', parts_in)
